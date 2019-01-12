978a85457f34c44c3468f640f6cc1df909e211ac
tomerberman <tomerberman@gmail.com>
2019-01-12 22:46:56 +0200

    search term from input element

--- js/main.js
+++ js/main.js
@@ -1,19 +1,61 @@
 'use strict'
 
-var moviePrm = fetch('http://www.omdbapi.com/?apikey=d777cf78&s=love&type=movie&page=1');
-var data = moviePrm.then(res => {
-    return res.json();
-})
-.then(resJson => {
-    console.log('data from api = ',resJson.Search);
-    renderList(resJson.Search, 'Title');
+var gState = {
+    list: [],
+    selected: null,
+    page: 1
+}
+const SEARCH_TERM_MIN_MENGTH = 3;
+
+document.querySelector('form.search').addEventListener('submit', ev => {
+    ev.preventDefault();
+    searchQuery();
 })
 
-function renderList(list, key){
+function searchOnline(query) {
+    var moviePrm = fetch(`http://www.omdbapi.com/?apikey=d777cf78&s=${query}&type=movie&page=${gState.page}`);
+    moviePrm.then(res => {
+        return res.json();
+    }).then(resJson => {
+        gState.list = resJson.Search;
+        renderList();
+        
+        console.log('data from api = ', resJson.Search);
+    })
+}
+
+function searchQuery() {
+    var query = document.querySelector('.search .movie-name').value
+    console.log('query of input el=', query);
+
+    if (query.length >= SEARCH_TERM_MIN_MENGTH) {
+        searchOnline(query);
+    }  // else show message
+}
+
+function renderList() {
     var strHtml = '';
-    for (let i=0; i<list.length; i++){
-        strHtml += `<li>Movie ${i} : ${list[i][key]}</li>`
-        console.log('data[' + i + '] =',list[i][key]);
+    for (let i = 0; i < gState.list.length; i++) {
+        strHtml += `<li onclick="onMovieClicked(this,` + i
+            + `)">Movie ${i} Title: ${gState.list[i].Title}` +
+            `<img src="` + gState.list[i].Poster + `"></li>`
+            
+        console.log('data[' + i + '] =', gState.list[i].Title);
     }
     document.querySelector('ul').innerHTML = strHtml;
 }
+
+function onMovieClicked(elClicked, idx) {
+    gState.selected = idx;
+    var elList = document.querySelectorAll('li')
+    elList.forEach(el => {
+        (el === elClicked) ? el.classList.add('selected') : el.classList.remove('selected');
+    });
+
+    console.log('idx=', idx);
+    console.log('ev=', elClicked);
+    console.log('details=', gState.list[idx].Year);
+}
+
+
+
